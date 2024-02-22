e7ae4f1ea8c0fe94ca997f74ecc5c405724feb15
rahzex <zexprorah@gmail.com>
2024-02-22 23:32:01 +0530

    update

--- src/com/leetcode/easy/binarytree/MaxDepthOfBinaryTree.java
+++ src/com/leetcode/easy/binarytree/MaxDepthOfBinaryTree.java
@@ -2,12 +2,10 @@ package com.leetcode.easy.binarytree;
 
 import com.leetcode.collections.TreeNode;
 
-// revise
-
 public class MaxDepthOfBinaryTree {
 
     public int maxDepth(TreeNode root) {
-        if(root==null){
+        if(root == null) {
             return 0;
         }
         return 1 + Math.max(maxDepth(root.left),maxDepth(root.right));
