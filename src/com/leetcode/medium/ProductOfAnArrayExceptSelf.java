b75392a6ad4480ac95c2b1873a2f171e2d937f73
rahzex <zexprorah@gmail.com>
2024-06-22 13:49:36 +0530

    update

--- src/com/leetcode/medium/ProductOfAnArrayExceptSelf.java
+++ src/com/leetcode/medium/ProductOfAnArrayExceptSelf.java
@@ -3,6 +3,29 @@ package com.leetcode.medium;
 import java.util.Arrays;
 
 public class ProductOfAnArrayExceptSelf {
+    public int[] productExceptSelfBetter(int[] nums) {
+        int[] prefix = new int[nums.length];
+        int[] suffix = new int[nums.length];
+        int[] product = new int[nums.length];
+
+        prefix[0] = 1;
+        suffix[nums.length - 1] = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i-1] * nums[i-1];
+        }
+
+        for (int j = nums.length - 2; j >= 0; j--) {
+            suffix[j] = suffix[j+1] * nums[j+1];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            product[i] = prefix[i] * suffix[i];
+        }
+
+        return product;
+    }
+
     public int[] productExceptSelf(int[] nums) {
         int[] fp = new int[nums.length];
         int[] bp = new int[nums.length];
